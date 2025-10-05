518a0a10b05ce251a32f058bed22313bfa3f08e1
Matyas Mandelicek <maty2107@gmail.com>
2025-10-05 16:00:05 +0200

    Editing

--- weekfitter-frontend/src/pages/LoginPage.js
+++ weekfitter-frontend/src/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
-import Logo from "../assets/Logo01.png";
+import Logo from "../assets/Logo02.png";
 import { useNavigate } from "react-router-dom";
 import "../styles/LoginPage.css";
 
