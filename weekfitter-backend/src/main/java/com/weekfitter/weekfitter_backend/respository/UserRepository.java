06eb17c4ad687953b65a962396c430cc3a564c44
Matyas Mandelicek <maty2107@gmail.com>
2025-10-15 16:51:44 +0200

    Změny

--- weekfitter-backend/src/main/java/com/weekfitter/weekfitter_backend/respository/UserRepository.java
+++ weekfitter-backend/src/main/java/com/weekfitter/weekfitter_backend/respository/UserRepository.java
@@ -2,7 +2,6 @@ package com.weekfitter.repository;
 
 import com.weekfitter.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
